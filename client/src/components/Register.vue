deded86085727d9efe44bf3709a1a1f6ecd06355
Junhao Wang <490107350@qq.com>
2018-06-13 20:29:41 +0800

    setting up registration function

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -14,6 +14,8 @@
           v-model="password"
           placeholder="password">
         <br>
+        <div v-if="isRegister" class="success">Register Success!</div>
+        <div v-html="error" class="error" v-if="isError"></div>
         <button
           @click="register"
           >Register</button>
@@ -26,20 +28,34 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null,
+      isRegister: false,
+      isError: true
     }
   },
   methods: {
     async register () {
-      await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+        this.isRegister = true
+        this.isError = false
+      } catch (error) {
+        this.error = error.response.data.error
+        this.isRegister = false
+        this.isError = true
+      }
     }
   }
 }
 </script>
 
 <style>
-
+.error,
+.success {
+  color:red;
+}
 </style>
